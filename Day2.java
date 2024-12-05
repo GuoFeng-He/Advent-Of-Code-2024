ab9d2a19f163e9c350d4421c709c7186f5804c2a
GuoFeng-He <145706759+GuoFeng-He@users.noreply.github.com>
2024-12-04 22:33:54 -0500

    Add files via upload

--- Day2.java
+++ Day2.java
@@ -1,60 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-
-// 743, 444, 442
 
 public class Day2 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Day2Input.txt");
-        ArrayList<int[]> list = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         for (String str: fileData){
             String[] splitString = str.split(" ");
-            int[] smallList = new int[splitString.length];
+            ArrayList<Integer> smallList = new ArrayList<>();
 
             for (int i = 0; i < splitString.length; i++){
-                smallList[i] = Integer.parseInt(splitString[i]);
+                smallList.add(Integer.parseInt(splitString[i]));
             }
             list.add(smallList);
         }
 
-        int count = 0;
-        for (int[] intList: list){
-            int lastDifference = 0;
-            int[] original = intList;
-            boolean strike = false;
-            boolean strikeTwo = false;
-
-            for (int i = 0; i < intList.length - 1; i++){
-                int difference = intList[i + 1] - intList[i];
-
-                if (difference * lastDifference >= 0 && Math.abs(difference) >= 1 && Math.abs(difference) <= 3){
-                    lastDifference = difference; // between the current idx and the next idx
-                } else {
-                    if (!strike){
-                        intList = removeElement(i, intList);
-                        strike = true;
-                        i--;
-                    } else {
-                        if (!strikeTwo){
-                            intList = removeFirstElement(original);
-                            strikeTwo = true;
-                            i = 0;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-
-                if (i == intList.length - 2){
-                    count++;
-                }
-            }
-        }
-        System.out.println(count);
+        System.out.println(partTwo(list));
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -74,30 +37,50 @@ public class Day2 {
         }
     }
 
-    public static int[] removeElement(int idx, int[] list){
-        int[] newList = new int[list.length - 1];
-        int increment = 0;
-
-        for (int i = 0; i < list.length; i++){
-            if (i != idx){
-                newList[i - increment] = list[i];
-            }else{
-                increment = 1;
+    public static int partOne(ArrayList<ArrayList<Integer>> list){
+        int count = 0;
+        for (ArrayList<Integer> intList: list){
+            if (isSafe(intList)){
+                count++;
             }
         }
-        return newList;
+        return count;
     }
-    public static int[] removeFirstElement(int[] list){
-        int[] newList = new int[list.length - 1];
-        int increment = 0;
 
-        for (int i = 0; i < list.length; i++){
-            if (i != 0){
-                newList[i - increment] = list[i];
-            } else{
-                increment = 1;
+    public static boolean isSafe(ArrayList<Integer> list){
+        int lastDifference = 0;
+
+        for (int i = 0; i < list.size() - 1; i++){
+            int difference = list.get(i + 1) - list.get(i);
+            if (difference * lastDifference >= 0 && Math.abs(difference) >= 1 && Math.abs(difference) <= 3){
+                lastDifference = difference;
+            } else {
+                break;
+            }
+
+            if (i == list.size() - 2){
+                return true;
             }
         }
-        return newList;
+        return false;
+    }
+
+    public static int partTwo(ArrayList<ArrayList<Integer>> list){
+        int count = 0;
+        for (ArrayList<Integer> intList: list){
+            for (int i = 0; i < intList.size(); i++){
+                ArrayList<Integer> tempList = new ArrayList<>();
+                for (int num: intList){
+                    tempList.add(num);
+                }
+                tempList.remove(i);
+
+                if (isSafe(tempList)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
